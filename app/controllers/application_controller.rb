dfbc8af0689e29f5fb48559679ee1ef08438511e
Quinn Mullaney <skiingqm@gmail.com>
2021-09-04 19:41:19 -0400

    working on user auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,29 @@
 class ApplicationController < ActionController::Base
+
+    helper_method :logged_in?, :current_user
+
+    def current_user
+        @current_user ||= User.find_by(session_token: session[:session_token])
+    end
+
+    def logged_in?
+        !!current_user
+    end
+
+    def log_in!(user)
+        session[:session_token] = user.reset_session_token!
+        @current_user = user
+    end
+
+    def logout
+        session[:session_token] = nil
+        current_user.reset_session_token!
+        current_user = nil
+    end
+
+
+    # not sure if i need this, proobably not 
+    def ensure_logged_in
+        redirect_to new_session_url if !logged_in?
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,41 @@
 class User < ApplicationRecord
+    validates :name, :email, :password_digest, :session_token, presence: true
+    validates :email, uniqueness: true
+    validates :password, length: { minimum: 6 }, allow_nil: true
+
+    after_initialize :ensure_session_token
+
+    def self.find_by_credentials(email, password)
+        user = User.find_by(email: email)
+        return nil if user.nil?
+        if user.is_password?(password)
+            return user
+        else
+            return nil
+        end
+    end
+
+    def password
+        @password 
+    end
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def is_password?(password)
+        BCrypt::Password.new(self.password_digest).is_password?(password)
+    end
+
+    def ensure_session_token
+        self.session_token ||= SecureRandom.urlsafe_base64
+    end
+
+    def reset_session_token!
+        self.session_token = SecureRandom.urlsafe_base64
+        self.save!
+        self.session_token 
+    end
+
 end
